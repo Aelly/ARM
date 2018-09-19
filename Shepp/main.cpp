914b44c583083b35e020a1c80560093d81119719
Charlie Schlick <cschlick@emi.u-bordeaux1.fr>
2018-09-19 14:29:45 +0200

    .

--- Shepp/main.cpp
+++ Shepp/main.cpp
@@ -1,9 +1,14 @@
 #include "mainwindow.h"
 #include <QApplication>
 
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QFile file("../../shepp_logan.pgm3d");
+
+    qDebug() << QT_VERSION_STR;
     MainWindow w;
     w.show();
 
