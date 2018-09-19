4d82eb36a72191816317600d19392e20c844d2af
Charlie Schlick <cschlick@emi.u-bordeaux1.fr>
2018-09-19 15:12:32 +0200

    openGl integration

--- Shepp/mainwindow.cpp
+++ Shepp/mainwindow.cpp
@@ -2,13 +2,56 @@
 #include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
+    myGLWidget(60, parent, "Premier Polygone avec OpenGL et Qt")
 {
-    ui->setupUi(this);
+
+}
+
+// Based on https://fhalgand.developpez.com/tutoriels/qt/integration-opengl/01-premiere-application/
+
+void MainWindow::initializeGL()
+{
+    glShadeModel(GL_SMOOTH);
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glClearDepth(1.0f);
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
+    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+}
+
+
+
+void MainWindow::resizeGL(int width, int height)
+{
+    if(height == 0)
+        height = 1;
+    glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(45.0f, (GLfloat)width/(GLfloat)height, 0.1f, 100.0f);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+}
+
+void MainWindow::paintGL()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+    glTranslatef(-1.5f, 0.0f, -6.0f);
+
+    glBegin(GL_TRIANGLES);
+        glVertex3f(0.0f, 1.0f, 1.0f);
+        glVertex3f(-1.0f, -1.0f, 0.0f);
+        glVertex3f(1.0f, -1.0f, -1.0f);
+    glEnd();
+
+    glTranslatef(3.0f, 0.0f, -6.0f);
+
+    glBegin(GL_TRIANGLES);
+        glVertex3f(1.0f,0.0f,-3);
 }
 
 MainWindow::~MainWindow()
 {
-    delete ui;
+
 }
--- Shepp/mainwindow.h
+++ Shepp/mainwindow.h
@@ -1,20 +1,21 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include <QMainWindow>
-#include <QtGui>
-#include <QFile>
+#include "myglwidget.h"
 
 namespace Ui {
 class MainWindow;
 }
 
-class MainWindow : public QMainWindow
+class MainWindow : public myGLWidget
 {
     Q_OBJECT
 
 public:
     explicit MainWindow(QWidget *parent = 0);
+    void initializeGL();
+    void resizeGL(int width, int height);
+    void paintGL();
     ~MainWindow();
 
 private:
